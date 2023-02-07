892fab631fe82d6a517d9436cc8faa8636f2a7b4
Arun Mudgal <58321357+Ar-cell@users.noreply.github.com>
2023-02-07 16:35:35 +0530

    Add feign client

--- src/main/java/com/inventory/management/InventoryManagementSystemApplication.java
+++ src/main/java/com/inventory/management/InventoryManagementSystemApplication.java
@@ -2,8 +2,12 @@ package com.inventory.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+//@EnableFeignClients   feign client is not in use for now.
 public class InventoryManagementSystemApplication {
 
 
